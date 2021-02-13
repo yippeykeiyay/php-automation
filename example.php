64be0ca39b876673b311d6833564da588dd40797
James Tyler <james.tyler@rapidspike.com>
2021-02-13 11:00:07 +0000

    new test bootstrapping class and updated example script to use it

--- example.php
+++ example.php
@@ -1,74 +1,38 @@
 <?php
 
-use Application\Library\ChromeOptions;
-use Application\Library\Proxy;
-use Application\Library\Resolution;
+require_once(__DIR__ . '/vendor/autoload.php');
+
 use Application\Library\Utils;
-use Application\Library\ViewPort;
-use Application\Library\WebDriverSession;
-use Application\Library\WebDriverSettings;
-use Facebook\WebDriver\Remote\WebDriverCapabilityType;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
-use RapidSpike\Targets\Url;
 
-require_once(__DIR__ . '/vendor/autoload.php');
-date_default_timezone_set('UTC');
-$start = microtime(true);
-
-// Servers
-$ProxyUrl = new Url('http://localhost:8080');
-$SeleniumUrl = new Url('http://localhost:4444/wd/hub');
-
-// Timeouts
-$timeout_seconds = 45;
-
-// Window/Resolution settings
-$Resolution = new Resolution(1920, 1080); /* your screen res */
-$ViewPort = new ViewPort(1440, 900, $Resolution); /* desired browser viewport */
-
-// Setup a Proxy Client
-$Proxy = new Proxy($ProxyUrl);
-
-// Setup Chrome
-// Add 'headless' to the `addOptions()` function in order to prevent Chrome opening
-$ChromeOptions = (new ChromeOptions)->addOption();
-
-// Setup WebDriver
-$WebDriverSettings = new WebDriverSettings($ChromeOptions, $Resolution);
-
-// Apply a proxy
-$WebDriverSettings->setCapability(WebDriverCapabilityType::PROXY, [
-    'proxyType' => 'manual',
-    'httpProxy' => $Proxy->getClient()->url,
-    'sslProxy' => $Proxy->getClient()->url
-]);
-
-// Start the remote session
-$WebDriverSession = WebDriverSession::generate($SeleniumUrl, $WebDriverSettings, $ViewPort, $timeout_seconds);
+// @TODO create a config class to send to the TestStrap construct
+$Test = new Application\TestStrap();
 
 /*
  * Run the actual test
  */
 try {
+    $Test->buildEnvironment();
+
     Utils::out("Opening a HAR");
-    $Proxy->newHar('https://www.tyler-architect.co.uk/');
+    $Test->getProxy()->newHar('https://www.tyler-architect.co.uk/');
 
     Utils::out("Loading page");
-    $WebDriverSession->get('https://www.tyler-architect.co.uk/');
+    $Test->getRemoteWebDriver()->get('https://www.tyler-architect.co.uk/');
     Utils::rest(1);
 
     // Wait for the iFrame to load
     Utils::out("Waiting for the something to be visible");
-    $WebDriverSession->wait($timeout_seconds)->until(
+    $Test->getRemoteWebDriver()->wait($Test->timeout_seconds)->until(
         WebDriverExpectedCondition::visibilityOfElementLocated(
             WebDriverBy::cssSelector('figure.logo')
         )
     );
 
     // Print the title and URI of the current page
-    Utils::out("The title is {$WebDriverSession->getTitle()}");
-    Utils::out("The current URI is {$WebDriverSession->getCurrentURL()}");
+    Utils::out("The title is {$Test->getRemoteWebDriver()->getTitle()}");
+    Utils::out("The current URI is {$Test->getRemoteWebDriver()->getCurrentURL()}");
 
 } catch (Exception $e) {
     Utils::out("Running Error! {$e->getMessage()}");
@@ -76,15 +40,12 @@ try {
 
 } finally {
     // Store the HAR
-    $path = $Proxy->storeHar(__DIR__ . '/output/hars/', 'tyler-architect');
+    $path = $Test->getProxy()->storeHar(__DIR__ . '/output/hars/', 'tyler-architect');
     if ($path) {
         Utils::out($path);
     }
 
     // Close the browser
-    $WebDriverSession->quit();
+    $Test->getRemoteWebDriver()->quit();
 }
 
-$run_time_s = round((microtime(true) - $start), 3, PHP_ROUND_HALF_UP);
-Utils::out("Completed in {$run_time_s} seconds");
-exit;
---
+++ source/Application/TestStrap.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Application;
+
+use Application\Library\ChromeOptions;
+use Application\Library\Proxy;
+use Application\Library\Resolution;
+use Application\Library\Utils;
+use Application\Library\ViewPort;
+use Application\Library\WebDriverSession;
+use Application\Library\WebDriverSettings;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\WebDriverCapabilityType;
+use RapidSpike\Targets\Url;
+
+date_default_timezone_set('UTC');
+
+/**
+ * Class TestStrap for bootstrapping everything required to run a test
+ *
+ * @package Application
+ */
+class TestStrap
+{
+
+    /**
+     * @var float|null
+     */
+    private static $_start = null;
+
+    /**
+     * @var Proxy
+     */
+    protected $Proxy;
+
+    /**
+     * @var RemoteWebDriver
+     */
+    protected $RemoteWebDriver;
+
+    public $timeout_seconds = 45;
+    public $res_width = 1920;
+    public $res_height = 1080;
+    public $vp_width = 1440;
+    public $vp_height = 900;
+    public $proxy_url = 'http://localhost:8080';
+    public $selenium_url = 'http://localhost:4444/wd/hub';
+
+    /**
+     * TestStrap constructor.
+     * Start the timer.
+     */
+    public function __construct()
+    {
+        self::$_start = microtime(true);
+    }
+
+    /**
+     * End the timer.
+     */
+    public function __destruct()
+    {
+        $run_time_s = round((microtime(true) - self::$_start), 3, PHP_ROUND_HALF_UP);
+        Utils::out("Completed in {$run_time_s} seconds");
+    }
+
+    /**
+     * @param array $arrChromeOptions
+     * @return $this
+     */
+    public function buildEnvironment(array $arrChromeOptions = []): TestStrap
+    {
+        // Setup a Proxy Client
+        $this->Proxy = new Proxy(new Url($this->proxy_url));
+
+        // Window resolution settings
+        $Resolution = new Resolution($this->res_width, $this->res_height); /* your screen res */
+
+        // Start the remote session
+        $this->RemoteWebDriver = WebDriverSession::generate(
+            new Url($this->selenium_url),
+            $this->_buildWebDriverSettings((new ChromeOptions)->addOptions($arrChromeOptions), $Resolution),
+            new ViewPort($this->vp_width, $this->vp_height, $Resolution),
+            $this->timeout_seconds
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param ChromeOptions $ChromeOptions
+     * @param Resolution $Resolution
+     * @return WebDriverSession
+     */
+    private function _buildWebDriverSettings(ChromeOptions $ChromeOptions, Resolution $Resolution): WebDriverSettings
+    {
+        // Setup WebDriver
+        $WebDriverSettings = new WebDriverSettings($ChromeOptions, $Resolution);
+
+        // Apply a proxy
+        $WebDriverSettings->setCapability(WebDriverCapabilityType::PROXY, [
+            'proxyType' => 'manual',
+            'httpProxy' => $this->Proxy->getClient()->url,
+            'sslProxy' => $this->Proxy->getClient()->url
+        ]);
+
+        return $WebDriverSettings;
+    }
+
+    /**
+     * @return Proxy
+     */
+    public function getProxy(): Proxy
+    {
+        return $this->Proxy;
+    }
+
+    /**
+     * @return RemoteWebDriver
+     */
+    public function getRemoteWebDriver(): RemoteWebDriver
+    {
+        return $this->RemoteWebDriver;
+    }
+
+}
